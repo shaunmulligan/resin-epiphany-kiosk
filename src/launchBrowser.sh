ca3dfd0962fe3f521f89dc8951682c9da5fe6fcd
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-26 14:34:49 +0100

    URL is now settable from Env Var, installed extra deps to try get full screen

--- src/launchBrowser.sh
+++ src/launchBrowser.sh
@@ -6,7 +6,9 @@
  xset s noblank
 
 mkdir /root/.config
-
+url=$URL
+default='https://html5test.com/'
 sudo matchbox-window-manager -use_cursor no -use_titlebar no &
-epiphany-browser -a --profile /root/.config https://html5test.com/ --display=:0
+xte 'sleep 10' 'key F11'&
+epiphany-browser -a --profile /root/.config ${url:-$default} --display=:0
 sleep 2s
